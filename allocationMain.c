3944c72e9361223dcbb8ebd32ad5f187ee57f2a0
David Estes McKnight <belmont95@live.com>
2017-12-08 03:11:23 -0600

    debug

--- allocationMain.c
+++ allocationMain.c
@@ -83,9 +83,11 @@ int main(int argc, char ** argv)
 
 				if (strcmp(args[0], "REQUEST")==0)
 				{
-
+					printf("Request method: \n");
+					printMemContents(mem);
 					firstFitProcess(&mem, atoi(args[2]), args[1]);
 					printMemContents(mem);
+					printf("Request method ended. \n");
 				}
 				else if (strcmp(args[0], "RELEASE") == 0)
 				{
