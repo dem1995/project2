fe1b4eac12f93dca18853d3751b8d36c61c81486
David Estes McKnight <belmont95@live.com>
2017-12-11 02:47:20 -0600

    buddyUpdate

--- fit_algos.c
+++ fit_algos.c
@@ -109,6 +109,7 @@ block* buddyFitProcess(memory* mem, unsigned long size, char* label)
 	}
 
 	block* spawnedProcess = spawnProcess(mem, blockToBreak, label, size);
+	buddyCleanMemory(*mem);
 
 	return spawnedProcess;
 }
