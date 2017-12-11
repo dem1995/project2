881d6a19d97f66f395c14d5793281f2ca2548fc8
David Estes McKnight <belmont95@live.com>
2017-12-11 06:00:42 -0600

    stf

--- block_buddy_methods.c
+++ block_buddy_methods.c
@@ -8,6 +8,8 @@ void splitBlock(block* blockToSplit)
 	block* sb2 = malloc(sizeof(block));
 	block* blockAfterSplit = blockToSplit->nextBlock;
 	*blockToSplit = createEmptyBlock(blockToSplit->size / 2, blockToSplit->prevBlock, sb2);
+	if (blockAfterSplit != NULL)
+		blockAfterSplit->prevBlock = sb2;
 	*sb2 = createEmptyBlock(blockToSplit->size, blockToSplit, blockAfterSplit);
 	//printf("Results: \n");
 	//printBlockContents(*sb2);
