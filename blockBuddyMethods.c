98520273de8a95f28449e55c3af70f0e24442310
David Estes McKnight <belmont95@live.com>
2017-12-11 02:57:29 -0600

    stf

--- blockBuddyMethods.c
+++ blockBuddyMethods.c
@@ -2,10 +2,16 @@
 
 void splitBlock(block* blockToSplit)
 {
+	printf("Splitting block: \n");
+	printBlockContents(*blockToSplit);
 	block* sb2 = malloc(sizeof(block));
 	block* blockAfterSplit = blockToSplit->nextBlock;
 	*blockToSplit = createEmptyBlock(blockToSplit->size / 2, blockToSplit->prevBlock, sb2);
 	*sb2 = createEmptyBlock(blockToSplit->size, blockToSplit, blockAfterSplit);
+	printf("Results: \n");
+	printBlockContents(*sb2);
+	printBlockContents(*blockToSplit);
+	printf("\n");
 }
 
 void splitBlockUntilPieceSize(block* blockToSplit, unsigned long size)
